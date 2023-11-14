fdbbac6601df1064b88845659726bce508edcd53
Elijah Davis <eli.davis1104@gmail.com>
2023-11-14 13:04:54 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,69 @@
+let gameHistory = `{"games":[{"turns":5,"answer":"about","solved":true,"player":"eb"},{"turns":6,"answer":"other","solved":false,"player":"jg"},{"turns":1,"answer":"which","solved":true,"player":"kc"},{"turns":1,"answer":"their","solved":true,"player":"ed"},{"turns":4,"answer":"there","solved":true,"player":"eb"},{"turns":4,"answer":"first","solved":true,"player":"jg"},{"turns":6,"answer":"would","solved":true,"player":"kc"},{"turns":6,"answer":"these","solved":false,"player":"ed"},{"turns":1,"answer":"click","solved":true,"player":"eb"},{"turns":6,"answer":"price","solved":true,"player":"jg"},{"turns":6,"answer":"state","solved":true,"player":"kc"},{"turns":5,"answer":"email","solved":true,"player":"ed"},{"turns":6,"answer":"world","solved":true,"player":"eb"},{"turns":1,"answer":"music","solved":true,"player":"jg"},{"turns":6,"answer":"after","solved":false,"player":"kc"},{"turns":6,"answer":"video","solved":true,"player":"ed"},{"turns":5,"answer":"where","solved":true,"player":"eb"},{"turns":4,"answer":"books","solved":true,"player":"jg"},{"turns":1,"answer":"links","solved":true,"player":"kc"},{"turns":6,"answer":"years","solved":false,"player":"ed"},{"turns":1,"answer":"order","solved":true,"player":"eb"},{"turns":6,"answer":"items","solved":false,"player":"jg"},{"turns":4,"answer":"group","solved":true,"player":"kc"},{"turns":5,"answer":"under","solved":true,"player":"ed"},{"turns":2,"answer":"games","solved":true,"player":"eb"},{"turns":6,"answer":"could","solved":false,"player":"jg"},{"turns":6,"answer":"great","solved":true,"player":"kc"},{"turns":4,"answer":"hotel","solved":true,"player":"ed"},{"turns":6,"answer":"store","solved":false,"player":"eb"},{"turns":4,"answer":"terms","solved":true,"player":"jg"},{"turns":4,"answer":"right","solved":true,"player":"kc"},{"turns":4,"answer":"local","solved":true,"player":"ed"},{"turns":4,"answer":"those","solved":true,"player":"eb"},{"turns":2,"answer":"using","solved":true,"player":"jg"},{"turns":3,"answer":"phone","solved":true,"player":"kc"},{"turns":1,"answer":"forum","solved":true,"player":"ed"},{"turns":6,"answer":"based","solved":false,"player":"eb"},{"turns":6,"answer":"black","solved":false,"player":"jg"},{"turns":6,"answer":"check","solved":true,"player":"kc"},{"turns":6,"answer":"index","solved":false,"player":"ed"},{"turns":1,"answer":"being","solved":true,"player":"eb"},{"turns":6,"answer":"women","solved":true,"player":"jg"},{"turns":5,"answer":"today","solved":true,"player":"kc"},{"turns":4,"answer":"south","solved":true,"player":"ed"},{"turns":6,"answer":"pages","solved":true,"player":"eb"},{"turns":6,"answer":"found","solved":false,"player":"jg"},{"turns":6,"answer":"house","solved":false,"player":"kc"},{"turns":6,"answer":"photo","solved":false,"player":"ed"},{"turns":6,"answer":"power","solved":false,"player":"eb"},{"turns":4,"answer":"while","solved":true,"player":"jg"},{"turns":5,"answer":"three","solved":true,"player":"kc"},{"turns":6,"answer":"total","solved":false,"player":"ed"},{"turns":6,"answer":"place","solved":true,"player":"eb"},{"turns":1,"answer":"think","solved":true,"player":"jg"},{"turns":3,"answer":"north","solved":true,"player":"kc"},{"turns":1,"answer":"posts","solved":true,"player":"ed"},{"turns":3,"answer":"media","solved":true,"player":"eb"},{"turns":2,"answer":"water","solved":true,"player":"jg"},{"turns":6,"answer":"since","solved":false,"player":"kc"},{"turns":6,"answer":"guide","solved":true,"player":"ed"},{"turns":1,"answer":"board","solved":true,"player":"eb"},{"turns":3,"answer":"white","solved":true,"player":"jg"},{"turns":6,"answer":"small","solved":false,"player":"kc"},{"turns":6,"answer":"times","solved":false,"player":"ed"},{"turns":1,"answer":"sites","solved":true,"player":"eb"},{"turns":4,"answer":"level","solved":true,"player":"jg"},{"turns":6,"answer":"hours","solved":false,"player":"kc"},{"turns":1,"answer":"image","solved":true,"player":"ed"},{"turns":3,"answer":"title","solved":true,"player":"eb"},{"turns":6,"answer":"shall","solved":false,"player":"jg"},{"turns":1,"answer":"class","solved":true,"player":"kc"},{"turns":2,"answer":"still","solved":true,"player":"ed"},{"turns":2,"answer":"money","solved":true,"player":"eb"},{"turns":6,"answer":"every","solved":false,"player":"jg"},{"turns":6,"answer":"visit","solved":true,"player":"kc"},{"turns":3,"answer":"tools","solved":true,"player":"ed"},{"turns":6,"answer":"reply","solved":false,"player":"eb"},{"turns":1,"answer":"value","solved":true,"player":"jg"},{"turns":5,"answer":"press","solved":true,"player":"kc"},{"turns":5,"answer":"learn","solved":true,"player":"ed"},{"turns":6,"answer":"print","solved":true,"player":"eb"},{"turns":6,"answer":"stock","solved":true,"player":"jg"},{"turns":6,"answer":"point","solved":false,"player":"kc"},{"turns":3,"answer":"sales","solved":true,"player":"ed"},{"turns":4,"answer":"large","solved":true,"player":"eb"},{"turns":4,"answer":"table","solved":true,"player":"jg"},{"turns":5,"answer":"start","solved":true,"player":"kc"},{"turns":5,"answer":"model","solved":true,"player":"ed"},{"turns":6,"answer":"human","solved":true,"player":"eb"},{"turns":6,"answer":"movie","solved":true,"player":"jg"},{"turns":4,"answer":"march","solved":true,"player":"kc"},{"turns":6,"answer":"yahoo","solved":true,"player":"ed"},{"turns":2,"answer":"going","solved":true,"player":"eb"},{"turns":4,"answer":"study","solved":true,"player":"jg"},{"turns":3,"answer":"staff","solved":true,"player":"kc"},{"turns":6,"answer":"again","solved":false,"player":"ed"},{"turns":6,"answer":"april","solved":false,"player":"eb"},{"turns":6,"answer":"never","solved":false,"player":"jg"},{"turns":5,"answer":"users","solved":true,"player":"kc"},{"turns":6,"answer":"topic","solved":true,"player":"ed"}]}`;
+let fourLetters = [
+  "pulp",
+  "fawn",
+  "free",
+  "lump",
+  "feel",
+  "felt",
+  "slot",
+  "size",
+];
+
+let fiveLetters = ["limit", "admin", "cabin", "scarf", "shock"];
+
+let sixLetters = [
+  "relent",
+  "morals",
+  "chords",
+  "untrue",
+  "jester",
+  "nieces",
+  "mobile",
+  "gaming",
+  "slopes",
+  "ambush",
+  "cynics",
+  "cobras",
+  "hurled",
+];
+
+let sevenLetters = [
+  "decoder",
+  "chevron",
+  "rallied",
+  "gravest",
+  "violate",
+  "ditched",
+  "dandily",
+  "chaffed",
+  "nosiest",
+];
+
+let eightLetters = [
+  "furuncle",
+  "tetotums",
+  "ferrymen",
+  "prefects",
+  "pedicabs",
+  "fetidity",
+  "superhit",
+  "kitenges",
+  "voidness",
+  "bumbling",
+  "cherubim",
+  "huskiest",
+  "unwarped",
+  "punaluan",
+  "ripienos",
+  "nautches",
+  "interact",
+  "sheeters",
+  "besieger",
+  "schemata",
+  "injoints",
+  "remended",
+  "doucines",
+  "snottily",
+  "damagers",
+];
